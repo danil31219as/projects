369d392573d0b7a7b2b62562efe5ee0eadc21daf
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-03-16 17:46:05 +0400

    получение всех работ

--- data/jobs.py
+++ data/jobs.py
@@ -1,9 +1,12 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
 from .db_session import SqlAlchemyBase
 
 
-class Jobs(SqlAlchemyBase):
+class Jobs(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'jobs'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
