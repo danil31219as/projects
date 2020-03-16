49e80e640d7a19f590ef65aafb3520ccdf7329bf
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-03-16 17:09:01 +0400

    получение всех работ

---
+++ get_jobs.py
@@ -0,0 +1,23 @@
+import flask
+from flask import jsonify
+
+from data import db_session
+from data.jobs import Jobs
+
+blueprint = flask.Blueprint('jobs_api', __name__,
+                            template_folder='templates')
+
+
+@blueprint.route('/api/jobs')
+def get_news():
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=(
+                'id', 'team_leader', 'title_of_activity', 'duration',
+                'list_of_collaborators', 'is_finished'))
+                 for item in jobs]
+        }
+    )
