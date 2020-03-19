6c222e9a4af03387f078d91fd84a87aed0623f4a
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-03-19 18:05:19 +0400

    RestFull start_test

--- test_parse.py
+++ test_parse.py
@@ -1,6 +1,6 @@
 from requests import post, get, delete
 
-print(post('http://localhost:5000/api/v2/users',
+print(post('http://localhost:5000/api/v2/users', #правильный
            json={'id': 2,
                  'name': 'tester',
                  'surname': 'abc',
@@ -12,6 +12,19 @@ print(post('http://localhost:5000/api/v2/users',
                  'hashed_password': '12345',
                  'modified_date': '01.01.01'
                  }).json())
-
-print(get('http://localhost:5000/api/v2/users').json())
-print(delete('http://localhost:5000/api/v2/users/2').json())
+print(post('http://localhost:5000/api/v2/users', # уже существует
+           json={'id': 1,
+                 'name': 'tester',
+                 'surname': 'abc',
+                 'age': '20',
+                 'position': '9_class',
+                 'speciality': 'no',
+                 'address': 'street',
+                 'email': 'test@test',
+                 'hashed_password': '12345',
+                 'modified_date': '01.01.01'
+                 }).json())
+print(get('http://localhost:5000/api/v2/users').json())# правильный
+print(get('http://localhost:5000/api/v2/users/65').json()) # нет такого id
+print(delete('http://localhost:5000/api/v2/users/2').json()) #правильный
+print(delete('http://localhost:5000/api/v2/users/40').json()) # нет такого id
